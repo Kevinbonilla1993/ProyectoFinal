8643fceba06da8ded58b31da609c1f4bb5691e3c
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-16 16:47:59 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Alertas Sismicas",
 result = st.experimental_get_query_params()
 print(result)
 
-country = result['pais'][0]
+country = result['val'][0]
 latitude = result['val'][1]
 longitude = result['val'][2]
 depth = result['val'][3]
