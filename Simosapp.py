f59c5785b41d1b7325113a5ce1986579e20e5293
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-16 16:54:57 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -8,14 +8,12 @@ st.set_page_config(page_title="Alertas Sismicas",
 
 # Obtener parámetros de la URL
 result = st.experimental_get_query_params()
-country = result.get('val', [])[0]
 country = result['val'][0]
-latitude = result.get('val', [])[1]
 latitude = result['val'][1]
-longitude = result.get('val', [])[2]
-depth = result.get('val', [])[3]
-mag = result.get('val', [])[4]
-sistype = result.get('val', [])[5]
+longitude=result['val'][2]
+depth = result['val'][3]
+mag = result['val'][4]
+sistype = result['val'][5]
 
 # Crear diseño
 if sistype == 'leve':
