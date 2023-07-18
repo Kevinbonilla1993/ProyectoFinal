a846cbfc16cac706e601a5706a12666e28eeef14
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-17 23:58:53 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -56,14 +56,14 @@ def show_details():
 col1, col2 = st.columns(2)
 with col1:
     # Crear el mapa de Folium
-    m = folium.Map(location=[latitude, longitude], zoom_start=3)
+    m = folium.Map(location=[latitude, longitude], zoom_start=6)
     
     # Agregar marcador al mapa
     popup_content = f"Magnitud: {mag} | Profundidad: {depth} km"
     folium.Marker([latitude, longitude], popup=popup_content).add_to(m)
     
-    # Mostrar el mapa en Streamlit usando folium_static
-    st.write(folium_static(m))
+    # Mostrar el mapa en Streamlit utilizando st.pydeck_chart()
+    st.pydeck_chart(folium_static(m))
 
 with col2:
     show_details()
