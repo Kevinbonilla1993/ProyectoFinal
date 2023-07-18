e29db3003cd56a0300549cdcbbed66b091387308
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-18 00:36:45 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -15,52 +15,6 @@ mag = result['val'][4]
 sistype = result['val'][5]
 fecha = result['val'][6]
 
-
-# Configuración de la página
-st.set_page_config(page_title="QuakeAlert", page_icon="🌍", layout="wide")
-
-# Título de la app
-st.title("QuakeAlert")
-
-# Agregar una descripción breve
-st.markdown('**Esta aplicación proporciona información detallada sobre sismos.**')
-
-# Crear un mapa centrado en la ubicación del sismo
-mapa = folium.Map(location=[sismo['latitude'], sismo['longitude']], zoom_start=10)
-
-# Estilo personalizado para el círculo del sismo
-def style_function(feature):
-    return {
-        'fillColor': 'red',
-        'color': 'black',
-        'weight': 2,
-        'fillOpacity': 0.7,
-    }
-
-# Añadir un marcador en la ubicación del sismo
-folium.Marker(location=[sismo['latitude'], sismo['longitude']], popup="Ubicación del sismo").add_to(mapa)
-
-# Añadir círculo en la ubicación del sismo con estilo personalizado
-folium.CircleMarker(location=[sismo['latitude'], sismo['longitude']], radius=10, popup="Magnitud: " + str(sismo['mag']),
-                    fill_color='red', color='black', fill_opacity=0.7).add_to(mapa)
-
-# Mostrar el mapa en Streamlit
-folium_static(mapa)
-
-st.subheader("Detalles del sismo")
-
-# Función para mostrar los detalles del sismo
-def show_sismo_details():
-    st.write(f"País: {sismo['country']}")
-    st.write(f"Latitud: {sismo['latitude']}")
-    st.write(f"Longitud: {sismo['longitude']}")
-    st.write(f"Magnitud: {sismo['mag']}")
-    st.write(f"Profundidad: {sismo['depth']} km")
-    st.write(f"Tipo de sismo: {sismo['sistype']}")
-    st.write(f"Fecha: {sismo['fecha']}")
-
-# Mostrar los detalles del sismo
-show_sismo_details()
 # Configuración de la página
 st.set_page_config(page_title="QuakeAlert", page_icon="🌍", layout="wide")
 
@@ -91,6 +45,10 @@ mapa = folium.Map(location=[latitude, longitude], zoom_start=10)
 
 # Añadir un marcador en la ubicación
 folium.Marker(location=[latitude, longitude], popup="Mi ubicación").add_to(mapa)
+
+# Añadir círculo en la ubicación del sismo con estilo personalizado
+folium.CircleMarker(location=[sismo['latitude'], sismo['longitude']], radius=10, popup="Magnitud: " + str(sismo['mag']),
+                    fill_color='red', color='black', fill_opacity=0.7).add_to(mapa)
 
 # Mostrar el mapa en Streamlit
 folium_static(mapa)
