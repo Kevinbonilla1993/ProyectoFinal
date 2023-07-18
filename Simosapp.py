dae306f00b1cebfa696210f8e79ebe9e44f56a02
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-17 19:05:53 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -4,31 +4,19 @@ import folium
 # Obtener parámetros de la URL
 result = st.experimental_get_query_params()
 
-# Verificar si las variables están presentes en la URL
-if 'val' in result:
-    val_list = result['val']
+country = result[0]
+latitude = float(result[1])  # Convertir a número
+longitude = float(result[2])  # Convertir a número
+depth = result[3]
+mag = result[4]
+sistype = result[5]
+date = result[6]
 
-    if len(val_list) == 7:
-        country = val_list[0]
-        latitude = float(val_list[1])  # Convertir a número
-        longitude = float(val_list[2])  # Convertir a número
-        depth = val_list[3]
-        mag = val_list[4]
-        sistype = val_list[5]
-        date = val_list[6]
+# Crear el mapa con las coordenadas dadas
+mapa = folium.Map(location=[latitude, longitude], zoom_start=10)
 
-        # Crear el mapa con las coordenadas dadas
-        mapa = folium.Map(location=[latitude, longitude], zoom_start=10)
+# Agregar marcador para la ubicación dada
+folium.Marker([latitude, longitude], popup=f'{country}, {date}').add_to(mapa)
 
-        # Agregar marcador para la ubicación dada
-        folium.Marker([latitude, longitude], popup=f'{country}, {date}').add_to(mapa)
-
-        # Mostrar el mapa en Streamlit
-        st.write(mapa)
-    else:
-        st.write("URL mal formada. Asegúrate de proporcionar todos los valores necesarios.")
-else:
-    st.write("No se encontraron valores en la URL. Asegúrate de proporcionar los parámetros 'val' en la URL.")
-
-
-
+# Mostrar el mapa en Streamlit
+st.write(mapa)
