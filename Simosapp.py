b8c5c24e8eb5ac07c71b1460dd9cab89a8428dbe
Kevinbonilla1993 <123422237+Kevinbonilla1993@users.noreply.github.com>
2023-07-18 13:30:54 -0500

    Update Simosapp.py

--- Simosapp.py
+++ Simosapp.py
@@ -24,20 +24,24 @@ st.set_page_config(page_title="QuakeAlert", page_icon="🌍", layout="wide")
 col1, col2, col3 = st.columns([1, 2, 1])
 
 # Columna 1: Mostrar el GIF
-gif_path = "quake_alert.gif"
+gif_path = "quake_alert..gif"
 col1.image(gif_path,use_column_width=True)
 
 # Columna 2: Mostrar el nombre de la página y opciones de sismos
-col2.title("Nombre de la página")
-seleccion = col2.radio("Elige una opción:", ("Inicio", "Últimos sismos"))
+col2.title("🚀 QuakeAlert 🌎")
 
 # Columna 3: Mostrar el menú desplegable
-col3.title("Menú desplegable")
-opciones = col3.selectbox("Selecciona una opción:", ["Opción 1", "Opción 2", "Opción 3"])
+st.sidebar.title("Menú desplegable")
 
-# Carga del GIF desde el directorio local
-gif_path = "quake_alert.gif"
-st.image(gif_path,use_column_width=True)
+# Opciones del menú desplegable
+paginas = ["Inicio", "Últimos sismos"]
+pagina_seleccionada = st.sidebar.radio("Selecciona una opción:", paginas)
+
+# Contenido de la página seleccionada
+if pagina_seleccionada == "Inicio":
+    mostrar_inicio()
+elif pagina_seleccionada == "Últimos sismos":
+    continue
 
 # Estilo personalizado para el título de la app
 # st.title("🚀 QuakeAlert 🌎")
@@ -46,79 +50,80 @@ st.image(gif_path,use_column_width=True)
 # Separadores
 st.markdown("---")
 
-# Crear el mapa
-mapa = folium.Map(location=[latitude, longitude], zoom_start=10)
-
-# Agregar un marcador para mostrar la ubicación del sismo
-folium.Marker(location=[latitude, longitude], popup=f"Sismo en {country}\nMagnitud: {mag}\nFecha: {fecha}").add_to(mapa)
-
-# Mostrar el mapa interactivo
-folium_static(mapa)
-
-st.subheader("Detalles del sismo")
-
-# Función para mostrar detalles con un diseño más creativo
-def show_details():
-    st.subheader("Detalles Generales")
-    st.markdown("---")
-    st.write(f"🌍 **País:** {country}")
-    st.write(f"📍 **Latitud:** {latitude}")
-    st.write(f"📍 **Longitud:** {longitude}")
-
-    st.subheader("Tiempo desde el sismo")
-    timezone = pytz.timezone('America/Bogota')  # Cambia 'NombreDeTuZonaHoraria' por la zona horaria correspondiente
-    sismo_time = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
-    sismo_time = timezone.localize(sismo_time)
-    now = datetime.now(timezone)
-    time_diff = now - sismo_time
-    st.write(f"Ha pasado {time_diff.days} días, {time_diff.seconds // 3600} horas y {time_diff.seconds // 60} minutos desde el sismo.")
-
-# Función para mostrar detalles específicos con un diseño más creativo
-def show_details2():
-    st.subheader("Detalles Específicos")
-    st.markdown("---")
+def mostrar_inicio():
+    # Crear el mapa
+    mapa = folium.Map(location=[latitude, longitude], zoom_start=10)
     
-    st.write(f"🌋 **Magnitud:** {mag}")
-    st.progress(int(mag * 10))  # Agregar una barra de progreso para visualizar la magnitud
+    # Agregar un marcador para mostrar la ubicación del sismo
+    folium.Marker(location=[latitude, longitude], popup=f"Sismo en {country}\nMagnitud: {mag}\nFecha: {fecha}").add_to(mapa)
     
-    st.write(f"🌊 **Profundidad:** {depth} km")
-    st.progress(int(depth))  # Agregar una barra de progreso para visualizar la profundidad
+    # Mostrar el mapa interactivo
+    folium_static(mapa)
     
-    st.write(f"📅 **Tipo de sismo:** {sistype}")
-    st.write(f"⏰ **Fecha:** {fecha}")
-
-# Mostrar el mapa y los detalles
-col1, col2 = st.columns(2)
-with col1:
-    show_details()
+    st.subheader("Detalles del sismo")
     
-with col2:
-    show_details2()
-
-# Inicializar la sesión
-if 'comments' not in st.session_state:
-    st.session_state.comments = []
-
-if 'survey_responses' not in st.session_state:
-    st.session_state.survey_responses = []
-
-# Agregar una encuesta rápida sobre seguridad
-st.subheader("Encuesta de Seguridad")
-question1 = st.radio("¿Tienes un plan de evacuación en caso de sismo?", ("Sí", "No"))
-question2 = st.radio("¿Tienes un kit de emergencia preparado?", ("Sí", "No"))
-question3 = st.radio("¿Conoces los lugares seguros en tu hogar?", ("Sí", "No"))
-
-if st.button("Enviar encuesta"):
-    # Guardar las respuestas de la encuesta en la lista de respuestas de encuestas
-    st.session_state.survey_responses.append({
-        "Pregunta 1": question1,
-        "Pregunta 2": question2,
-        "Pregunta 3": question3
-    })
-
-st.subheader("Respuestas de Encuesta Guardadas")
-for response in st.session_state.survey_responses:
-    st.write(response)
+    # Función para mostrar detalles con un diseño más creativo
+    def show_details():
+        st.subheader("Detalles Generales")
+        st.markdown("---")
+        st.write(f"🌍 **País:** {country}")
+        st.write(f"📍 **Latitud:** {latitude}")
+        st.write(f"📍 **Longitud:** {longitude}")
+    
+        st.subheader("Tiempo desde el sismo")
+        timezone = pytz.timezone('America/Bogota')  # Cambia 'NombreDeTuZonaHoraria' por la zona horaria correspondiente
+        sismo_time = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+        sismo_time = timezone.localize(sismo_time)
+        now = datetime.now(timezone)
+        time_diff = now - sismo_time
+        st.write(f"Ha pasado {time_diff.days} días, {time_diff.seconds // 3600} horas y {time_diff.seconds // 60} minutos desde el sismo.")
+    
+    # Función para mostrar detalles específicos con un diseño más creativo
+    def show_details2():
+        st.subheader("Detalles Específicos")
+        st.markdown("---")
+        
+        st.write(f"🌋 **Magnitud:** {mag}")
+        st.progress(int(mag * 10))  # Agregar una barra de progreso para visualizar la magnitud
+        
+        st.write(f"🌊 **Profundidad:** {depth} km")
+        st.progress(int(depth))  # Agregar una barra de progreso para visualizar la profundidad
+        
+        st.write(f"📅 **Tipo de sismo:** {sistype}")
+        st.write(f"⏰ **Fecha:** {fecha}")
+    
+    # Mostrar el mapa y los detalles
+    col1, col2 = st.columns(2)
+    with col1:
+        show_details()
+        
+    with col2:
+        show_details2()
+    
+    # Inicializar la sesión
+    if 'comments' not in st.session_state:
+        st.session_state.comments = []
+    
+    if 'survey_responses' not in st.session_state:
+        st.session_state.survey_responses = []
+    
+    # Agregar una encuesta rápida sobre seguridad
+    st.subheader("Encuesta de Seguridad")
+    question1 = st.radio("¿Tienes un plan de evacuación en caso de sismo?", ("Sí", "No"))
+    question2 = st.radio("¿Tienes un kit de emergencia preparado?", ("Sí", "No"))
+    question3 = st.radio("¿Conoces los lugares seguros en tu hogar?", ("Sí", "No"))
+    
+    if st.button("Enviar encuesta"):
+        # Guardar las respuestas de la encuesta en la lista de respuestas de encuestas
+        st.session_state.survey_responses.append({
+            "Pregunta 1": question1,
+            "Pregunta 2": question2,
+            "Pregunta 3": question3
+        })
+    
+    st.subheader("Respuestas de Encuesta Guardadas")
+    for response in st.session_state.survey_responses:
+        st.write(response)
 
 
 
